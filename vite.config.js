66b9d59b9e96ddd8b1b4881adb74f71a4ecd3c81
mozart <239207200+JoinupRosario@users.noreply.github.com>
2025-11-15 11:57:30 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss],
+  plugins: [react()],
   base: "/",
   define: {
     global: 'window',
