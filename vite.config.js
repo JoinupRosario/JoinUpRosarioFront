2fa60e5a018da3162d6dc1979d0d0c9f11e168a9
mozart <239207200+JoinupRosario@users.noreply.github.com>
2025-11-15 11:59:06 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  define: {
-    global: 'window',
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  base: '/', 
 })
